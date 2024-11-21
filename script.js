93979eb05d20ec030d946f56386974060e945a55
VombatBigFinger <mityakalsin@gmail.com>
2024-11-21 11:57:24 +0300

    Добавить недостающие файлы

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
+import { OnboardingTour } from "./node_modules/light-onboarding-tour/OnboardingTour"
+
+
 function getVersion() {
     let version = localStorage.getItem('ab_version');
     if (!version) {
         version = Math.random() < 0.5 ? 'a' : 'b'; // С вероятностью 50% выбираем 'a' или 'b'
         localStorage.setItem('ab_version', version);
     }
+    console.log('Выбранная версия:', getVersion());
     return version;
 }
 
-// В зависимости от выбранной версии загружаем нужный контент
+
 function loadVersion() {
     const version = getVersion();
-    if (version === 'a') {
-        // Включаем версию с онбордингом
-        
-        // Подключаем и запускаем онбординг
-        import('./node_modules/light-onboarding-tour/OnboardingTour.js').then(({ OnboardingTour }) => {
-            const tour = new OnboardingTour();
 
-            tour.StartNewTour([
-                {
-                    element: '#el1',
-                    popover: {
-                        title: "Добро пожаловать",
-                        description: "ХАй ЗЯБЛ",
-                        position: 'right'
-                    }
-                }
-            ]);
-        });
+    if (version === 'a') {
+        // Запускаем библиотеку онбординга
+        const tour = new OnboardingTour();
+        tour.StartNewTour([
+            {
+                element: '#el1', // Существующий элемент в HTML
+                popover: {
+                    title: "Добро пожаловать",
+                    description: "Хай ЗЯБЛ",
+                    position: 'right',
+                },
+            },
+        ]);
     } else {
-        // Включаем версию без онбординга
-        document.getElementById('onboarding-container').innerHTML = '';
+        console.log('Версия B: без онбординга.');
     }
 
     // Отправляем данные о версии в Яндекс.Метрику
